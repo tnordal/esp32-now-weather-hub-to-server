64cf49c6252047653fd9718bbe49246b311ac1c9
Tom Nordal <tnordal@gmail.com>
2024-11-24 13:24:55 +0100

    Receive json as string correct

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,41 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define RXD2 16
+#define TXD2 17
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+String receivedMessage = "";
+
+void setup()
+{
+  // Note the format for setting a serial port is as follows: Serial2.begin(baud-rate, protocol, RX pin, TX pin);
+  Serial.begin(9600);
+
+  // Serial1.begin(9600, SERIAL_8N1, RXD2, TXD2);
+  Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
+  Serial.println("Serial Txd is on pin: " + String(TXD2));
+  Serial.println("Serial Rxd is on pin: " + String(RXD2));
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  // Choose Serial1 or Serial2 as required
+  while (Serial2.available())
+  {
+    char inCommingChar = char(Serial2.read());
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    if (inCommingChar == '\n')
+    {
+      // Print the message
+      Serial.print("You sent: ");
+      Serial.println(receivedMessage);
+
+      // Clear the message buffer for the next input
+      receivedMessage = "";
+    }
+    else
+    {
+      // Append the character to the message string
+      receivedMessage += inCommingChar;
+    }
+  }
 }
