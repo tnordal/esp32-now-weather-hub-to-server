9c01d0afdba4af36d65df0d3169aff56021835c9
Tom Nordal <tnordal@gmail.com>
2024-11-24 21:43:34 +0100

    setup wifi, mqtt and send json to server

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,41 @@
 #include <Arduino.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
+#include <WiFi.h>
 
+#include "secrets.h"
+
+const char MQTT_BROKER_ADRRESS[] = "192.168.3.33";
+const uint16_t MQTT_PORT = 1883;
+const char PUBLISH_WEATHER_OUTDOOR_TOPIC[] = "weather/outdoor/";
+
+// Create WiFi and MQTT client instances
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
 
 #define RXD2 16
 #define TXD2 17
 
 String receivedMessage = "";
 
+void setupWiFi();
 bool parseReceivedMessage(const String& receivedMessage, JsonDocument& doc);
+void reconnectMqttClient();
 
 void setup()
 {
   // Note the format for setting a serial port is as follows: Serial2.begin(baud-rate, protocol, RX pin, TX pin);
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   // Serial1.begin(9600, SERIAL_8N1, RXD2, TXD2);
   Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
   Serial.println("Serial Txd is on pin: " + String(TXD2));
   Serial.println("Serial Rxd is on pin: " + String(RXD2));
+
+  setupWiFi();
+
+  mqttClient.setServer(MQTT_BROKER_ADRRESS, MQTT_PORT);
+
 }
 
 void loop()
@@ -43,6 +60,19 @@ void loop()
         Serial.print("deviceID: ");
         Serial.println(deviceID);
 
+        // Publish the data to the MQTT broker
+        if (deviceID == "ESP32-1-outdoor")
+        {
+          // Publish the data to the MQTT broker
+          if (!mqttClient.connected())
+          {
+            reconnectMqttClient();
+          }
+          // mqttClient.publish(PUBLISH_WEATHER_OUTDOOR_TOPIC, receivedMessage.c_str());
+          mqttClient.publish(PUBLISH_WEATHER_OUTDOOR_TOPIC, receivedMessage.c_str());
+          Serial.println("Published data to MQTT broker");
+        }        
+
       }
       else {
         // Failed to parse JSON or 'deviceID' key does not exist
@@ -60,6 +90,34 @@ void loop()
   }
 }
 
+void setupWiFi(){
+    delay(10);
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(SSID);
+
+    WiFi.begin(SSID, PASSWORD);
+
+    unsigned long startAttemptTime = millis();
+    const unsigned long timeout = 2 * 60 * 1000; // 2 minutes in milliseconds
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        if (millis() - startAttemptTime >= timeout)
+        {
+            Serial.println("Failed to connect to WiFi within 2 minutes. Rebooting...");
+            ESP.restart();
+        }
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());  
+}
+
 bool parseReceivedMessage(const String& receivedMessage, JsonDocument& doc) {
   DeserializationError error = deserializeJson(doc, receivedMessage);
   if (error) {
@@ -74,4 +132,26 @@ bool parseReceivedMessage(const String& receivedMessage, JsonDocument& doc) {
   }
 
   return true;
+}
+
+void reconnectMqttClient()
+{
+    // Loop until we're reconnected
+    while (!mqttClient.connected())
+    {
+        Serial.println("Attempting MQTT connection...");
+        // Attempt to connect
+        if (mqttClient.connect("ESP32Client"))
+        {
+            Serial.println("connected");
+        }
+        else
+        {
+            Serial.print("failed, rc=");
+            Serial.print(mqttClient.state());
+            Serial.println(" try again in 5 seconds");
+            // Wait 5 seconds before retrying, use mills() to avoid blocking
+            delay(5000);
+        }
+    }
 }
